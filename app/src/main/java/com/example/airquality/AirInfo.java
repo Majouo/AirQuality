ae6edbc47264d23befe909a0ee3d9f73da29a6aa
Michał Maj <Mich180@outlook.com>
2023-05-31 13:46:03 +0200

    User localization and UI of quality meter

---
+++ app/src/main/java/com/example/airquality/AirInfo.java
@@ -0,0 +1,48 @@
+package com.example.airquality;
+
+import java.io.Serializable;
+
+public class AirInfo implements Serializable {
+    private String name;
+    private String description;
+    private int level;
+
+    public AirInfo(String name, String description, int level) {
+        this.name = name;
+        this.description = description;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "AirInfo{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", level=" + level +
+                '}';
+    }
+}
--- app/src/main/java/com/example/airquality/MainActivity.java
+++ app/src/main/java/com/example/airquality/MainActivity.java
@@ -1,14 +1,168 @@
 package com.example.airquality;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.location.LocationManager;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LocationRequest locationRequest;
+    private Location userLocation;
+    private static final int REQUEST_CHECK_SETTINGS = 10001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(2000);
+        fetchUserLocation();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.location:
+                if(fetchUserLocation()==true)
+                {
+                    System.out.println(userLocation);
+                    item.setIcon(getResources().getDrawable(R.drawable.baseline_location_on_24));
+                }
+                else
+                {
+                    item.setIcon(getResources().getDrawable(R.drawable.baseline_wrong_location_24));
+                }
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean fetchUserLocation()
+    {
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M)
+        {
+            if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            {
+                if(isGPSEnabled())
+                {
+                    LocationServices.getFusedLocationProviderClient(MainActivity.this).requestLocationUpdates(locationRequest, new LocationCallback() {
+                        @Override
+                        public void onLocationResult(@NonNull LocationResult locationResult) {
+                            super.onLocationResult(locationResult);
+
+                            LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                                    .removeLocationUpdates(this);
+                            if(locationResult!=null&&locationResult.getLocations().size()>0)
+                            {
+                                int index= locationResult.getLocations().size()-1;
+                                userLocation= new Location(locationResult.getLocations().get(index).getLongitude(),locationResult.getLocations().get(index).getLatitude());
+                            }
+                        }
+                    }, Looper.getMainLooper());
+                    if(userLocation!=null)
+                    {
+                        return true;
+                    }
+                }else
+                {
+                    turnOnGPS();
+                }
+            }else
+            {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+        return false;
+    }
+    private void turnOnGPS() {
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    Toast.makeText(MainActivity.this, "GPS is already tured on", Toast.LENGTH_SHORT).show();
+
+                } catch (ApiException e) {
+
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            //Device does not have location
+                            break;
+                    }
+                }
+            }
+        });
+    }
+
+
+    private boolean isGPSEnabled()
+    {
+        LocationManager locationManager = null;
+        boolean isEnabled=false;
+        if(locationManager==null) {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        }
+        isEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return isEnabled;
     }
 }
